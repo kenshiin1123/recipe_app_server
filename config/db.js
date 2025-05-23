1e2ceec1fbbc5dd2e9ae1b97434b0ce3e8d5c6a9
Lance Ivan Gil Fernandez <fernandezlanceivangil@gmail.com>
2025-05-23 09:26:57 +0800

    add mongodb

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
 
 const { connect, connection } = mongoose;
 
+const MONGODB_URL = process.env.MONGODB_URL;
+
 export default () => {
-  connect("mongodb://localhost:27017/recipe_app_db");
+  connect(MONGODB_URL);
   connection.on("error", console.error.bind("Connection Error"));
   connection.once("open", () => {
     console.log("Mongodb Connected!");
